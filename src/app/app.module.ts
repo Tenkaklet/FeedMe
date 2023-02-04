72cf260156220f1e0ddcadd77497530fa2671648
Max Carlquist <tenkaklet@gmail.com>
2023-02-04 12:51:27 +0100

    Added Angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule, HttpClientJsonpModule, HttpClient } from '@angular/common/http';
-
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { WelcomeComponent } from './welcome/welcome.component';
+import {firebase, firebaseui, FirebaseUIModule} from 'firebaseui-angular';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from './environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     HttpClientJsonpModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyBw6pYwOLZfAJhzL2Ulb_cVDfo-M5jMe8U",
+        authDomain: "feedme-8f76c.firebaseapp.com",
+        projectId: "feedme-8f76c",
+        storageBucket: "feedme-8f76c.appspot.com",
+        messagingSenderId: "180397014353",
+        appId: "1:180397014353:web:e48203d89ccb90a07ee770",
+        measurementId: "G-E6KFFN8C9J"
+      },
+  };
