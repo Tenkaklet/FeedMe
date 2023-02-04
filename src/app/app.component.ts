490cda3fcddfd352c5c3dfce7347e141f36ee17b
Max Carlquist <tenkaklet@gmail.com>
2023-02-04 14:37:11 +0100

    Added firebase-ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,18 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor() {}
+  constructor(public auth: AngularFireAuth) {
+    this.loggedIn();
+  }
+
+
+  loggedIn() {
+    this.auth.onAuthStateChanged(user => {
+      if (user) {
+        console.log('User is signed in');
+      } else {
+        console.log('User is not signed in');
+      }
+    })
+  }
 }
