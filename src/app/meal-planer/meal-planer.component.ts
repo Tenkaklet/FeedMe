684bf43e9d5d0bc948350e5aa08c6888af95345b
Max Carlquist <tenkaklet@gmail.com>
2023-02-20 10:48:59 +0100

    Implemented Edamam API

--- src/app/meal-planer/meal-planer.component.ts
+++ src/app/meal-planer/meal-planer.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EdamamService } from '../edamam.service';
 
 @Component({
   selector: 'app-meal-planer',
   templateUrl: './meal-planer.component.html',
   styleUrls: ['./meal-planer.component.scss']
 })
-export class MealPlanerComponent {
+export class MealPlanerComponent implements OnInit {
+  mealSelection = '';
+  
+  constructor(private edamam: EdamamService) {
+  }
 
+  ngOnInit(): void {
+    
+  }
+
+  onSelect(type: any) {
+    this.mealSelection = type.target.value;
+    this.getDataForMeal(this.mealSelection);
+  }
+
+  getDataForMeal(selection: string) {
+    console.info('the selection ', selection);
+    this.edamam.getRecipesByMeal(selection)
+    .subscribe(u => {
+      console.log('the edamam api data is ', u);
+    });
+  }
 }
